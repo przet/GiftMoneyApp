958318858a58be40245ed7a00684b9c6fa3c94f1
Philippe <p.rzetelski.note@gmail.com>
2018-04-29 00:02:17 +0930

    Found out what is wrong with subtask5 - after the first std::cin we end. But I am not sure why...will investigate.

--- readInDoubles.cpp
+++ readInDoubles.cpp
@@ -7,11 +7,6 @@
 
 int main()
 {
-    std::string mystr;
-    std::cout << "What is your name? " << std::endl;
-    getline(std::cin, mystr);
-    std::cout << "Hello " << mystr << std::endl;
-
     std::string filename = "amounts.txt";
     std::fstream amountsFile(filename, std::ios::in);
     if(amountsFile.fail())
@@ -33,7 +28,6 @@ int main()
     std::cout << number1 << std::endl;
     std::cout << number2 << std::endl;
     amountsFile.close();
-    
 
     //B:From input read in a value (double) such that:
     //if prev val - new val > 0, we replace the first line with result(prev val - new val)
@@ -44,10 +38,9 @@ int main()
 
     double inputVal;
     std::cout << "Enter a number to replace the first line of the file by " << std::endl;
-    std::cin >> inputVal;
+    std::cin >> inputVal; 
     
     amountsFile.open(filename,std::ios::out);
-    
     if(amountsFile.fail())
     {
         std::cerr <<"\nError!!\nThe file " <<filename 
@@ -62,22 +55,15 @@ int main()
     else amountsFile << 0 << '\n' << (lines[1] + newFirstLine) << '\n';
     
     amountsFile.close();
-    //B: Read in a description and append to bottom of the file
-
-    std::cout << "\n\n Please Enter a description : " << std::endl;
     
+    //B: Read in a description and append to bottom of the file
+    std::cout << "\n\nPlease Enter a description : " << std::endl;
     std::string description;
     getline(std::cin,description);
     std::cout << description << std::endl; 
     amountsFile.open(filename,std::ios::app);
     amountsFile << '\n' << description << '\n';
     amountsFile.close();
-     
-
-
-
-   
-
     std::cout << "Exiting..." << std::endl;
     return 0;
 }
